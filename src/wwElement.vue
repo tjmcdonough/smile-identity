858a409bf54176b82f715f8b5ebe07cd9239f389
Tom McDonough <tmcdonough@airslip.com>
2022-08-31 13:50:33 +0400

    Update wwElement.vue

--- src/wwElement.vue
+++ src/wwElement.vue
@@ -10,34 +10,30 @@
 
   export default {
     data: () => ({
-      //......data of your component
+      //......data of component
     }),
     mounted() {
       const app = document.querySelector('smart-camera-web');
 
   const postContent = async (data) => {
 
-    const imagesToSend = {
-      'image-one': data.images[0].image,
-      'image-two': data.images[1].image,
-      'image-three': data.images[2].image,
-      'image-four': data.images[3].image,
-      'image-five': data.images[4].image,
-      'image-six': data.images[5].image,
-      'image-seven': data.images[6].image,
-    }
+    // Send images to endpoint from data object
+
+    const imagesToSend = [
+      //data.images[i].image
+    ]
 
     const options = {
       method: 'POST',
       headers: {
         'Content-Type': 'multipart/form-data',
-        'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjYxODM0Nzc3LCJpYXQiOjE2NjE3NDgzNzcsImp0aSI6ImUxYzQ5ZDM3ZDhlYjQ2MjRhM2JmNGY5MTk5ZTdiZTNjIiwidXNlcl9pZCI6IjIxOWM4M2VjLWRmNjktNGQzOS1hYWQyLTlmODg4YzgwYTA4OSJ9.1G1CfgWAr8G3HNJcjYbXZ0NFOxpeJi_cZyKbcW3y0zw',
+        'Authorization': 'Bearer {GET_ACCESS_TOKEN_FROM_WEWEB}',
       },
       body: JSON.stringify(imagesToSend)
     };
 
     try {
-      const response = await fetch('http://bnkability-staging-001.eu-west-2.elasticbeanstalk.com/api/v1/account/selfies', options);
+      const response = await fetch('https://backend.bnkability.com/api/v1/account/selfies', options);
 
       if(response.status == 200 || response.status == 201)
       {
@@ -64,7 +60,7 @@
 
     },
     methods: {
-      //......methods of your component
+      //......methods of component
     }
   }
   
